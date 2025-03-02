a09e06f3b83846675eeffbf655e69ca9d324f0cc
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2025-03-02 15:03:03 +0530

    ci cd pipeline is working test 4

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1> Finally CI/CD is working for Sahil's Vite + React app</h1>
+      <h1> Finally secure CI/CD is working for Sahil's Vite + React app</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
