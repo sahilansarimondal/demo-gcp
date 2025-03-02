e45a5364e0152f4ace60bd9902df163b33ab63f1
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2025-03-02 19:04:17 +0530

    added pnpm and removed npm test 4

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Pnpm added for Sahil's Vite + React app</h1>
+      <h1>Pnpm added successfully for Sahil's Vite + React app</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
